095d6e08285590a372951cdaa13f0c7a6d492d48
cekhushisharma-source <ce.khushisharma@gmail.com>
2026-04-15 05:22:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let express= require("express")
-let cors=require("cors")
-
+let cors = require("cors")
+require("dotenv").config()
+let {GoogleGenerativeAI}=require("@google/generative-ai")
 let App=express()
+App.use(cors())  //Middleware
+App.use(express.json()) //store object from frontend to backend
+
+let genAI=new GoogleGenerativeAI(process.env.KEY) //key connected from env to this
+let model=genAI.getGenerativeModel({model:"gemini-2.5-flash"})
 
 
 
-
-
-App.listen("8000")
+App.listen(process.env.PORT,()=>{
+    console.log("Server Start");
+})
